354af23f6ab2c62895e63dd043cb9d5950eb45a0
Milan Humagain <10011027@BCS.NET.NZ>
2017-09-04 11:55:57 +1200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int n1,n2
+
+Console.Write("\n\n");
+    Console.Write("Find bigger number:\n");
+    
+    Console.Write("\n\n"); 
+    Console.Write("Input n1: ");  
+    n2= Convert.ToInt32(Console.ReadLine()); 
+    Console.Write("Input n2: ");  
+    n1 = Convert.ToInt32(Console.ReadLine()); 
+    Console.Write("\n\nThe bigger number is :\n"); 
+  
+       {
+
+       for (int i = 0; i <=count; i++)
+       } 
+  
+    if(n1==n2do)  
+     
+        Console.Write("{0}  ", n1); 
+  }  
+        Console.Write("\n\n");
+
